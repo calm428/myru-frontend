ca90b8de1d2d514ea929a1a9aeee7c3dc64027b7
calm428 <ollie21428@gmail.com>
2024-08-02 11:20:33 +0400

    fix for slider

--- components/home/flow/flow-image-gallery.tsx
+++ components/home/flow/flow-image-gallery.tsx
@@ -31,7 +31,7 @@ function FlowImageGallery({ images }: FlowImageGalleryProps) {
   return (
     <ImageGallery
       items={images}
-      additionalClass={'sliderH'}
+      // additionalClass={'sliderH'}
       thumbnailPosition={`${windowWidth > 640 ? 'right' : 'bottom'}`}
     />
   );
