60dcba32bda982959e16f17f333d5831b44a852e
calm428 <ollie21428@gmail.com>
2024-09-29 19:14:44 +0400

    test share button for ios 2

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,15 +1,21 @@
-// global.d.ts
+declare global {
+  interface Window {
+    ym: any;
+  }
 
-interface WebkitMessageHandlers {
-  shareHandler: {
-    postMessage: (message: string) => void;
-  };
+  interface WebkitMessageHandlers {
+    shareHandler: {
+      postMessage: (message: string) => void;
+    };
+  }
+
+  interface Webkit {
+    messageHandlers: WebkitMessageHandlers;
+  }
+
+  interface Window {
+    webkit?: Webkit;
+  }
 }
 
-interface Webkit {
-  messageHandlers: WebkitMessageHandlers;
-}
-
-interface Window {
-  webkit?: Webkit;
-}
+export {};
