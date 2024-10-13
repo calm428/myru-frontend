1198ba9ff2e10e59dc5acd8856ce5d47169aac50
calm428 <ollie21428@gmail.com>
2024-10-14 00:38:45 +0400

    products inside profile 22

--- app/[locale]/(protected)/profile/posts/MyProducts.tsx
+++ app/[locale]/(protected)/profile/posts/MyProducts.tsx
@@ -12,6 +12,8 @@ import { PostCardSkeleton } from '@/components/profiles/posts/post-card-skeleton
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import axios from 'axios';
+import { useLocale, useTranslations } from 'next-intl';
+import { useRouter, useSearchParams } from 'next/navigation';
 import toast from 'react-hot-toast';
 import useSWR from 'swr';
 import { PaginationComponent } from '@/components/common/pagination';
@@ -26,12 +28,18 @@ import {
   DialogDescription,
 } from '@/components/ui/dialog';
 
+// Функция для получения данных с API
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-
 const pageSize = 10;
 
 export default function MyPostsPage() {
-  const { user } = useContext(PaxContext); // Получаем данные пользователя
+  const { user } = useContext(PaxContext);
+  const locale = useLocale();
+  const t = useTranslations('main');
+  const tt = useTranslations('stream');
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
   const [maxPage, setMaxPage] = useState<number>(1);
   const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
   const [showArchiveModal, setShowArchiveModal] = useState<boolean>(false);
@@ -42,6 +50,7 @@ export default function MyPostsPage() {
   const [blogs, setBlogs] = useState<PostCardProps[]>([]);
   const [fetchURL, setFetchURL] = useState<string>('/api/flows/me');
   const [isConversionModalOpen, setIsConversionModalOpen] = useState(false); // Модальное окно для конвертации
+  const [isLoadingUser, setIsLoadingUser] = useState(true); // Состояние для отслеживания загрузки
 
   const {
     data: fetchedData,
@@ -50,10 +59,25 @@ export default function MyPostsPage() {
   } = useSWR(fetchURL, fetcher);
 
   useEffect(() => {
-    if (user?.seller) {
-      setFetchURL('/api/flows/me'); // Устанавливаем URL для получения постов продавца
-    }
-  }, [user]);
+    const generateFetchURL = () => {
+      const currentPage = searchParams.get('page')
+        ? Number(searchParams.get('page'))
+        : 1;
+      let baseURL = `/api/flows/me?language=${locale}&limit=${pageSize}&skip=${(currentPage - 1) * pageSize}`;
+      const queryParams = ['skip', 'limit', 'search', 'isArchive'];
+
+      queryParams.forEach((param) => {
+        const value = searchParams.get(param);
+        if (value) {
+          baseURL += `&${param}=${value}`;
+        }
+      });
+
+      return baseURL;
+    };
+
+    setFetchURL(generateFetchURL());
+  }, [searchParams, locale]);
 
   useEffect(() => {
     if (!error && fetchedData) {
@@ -61,6 +85,15 @@ export default function MyPostsPage() {
       setMaxPage(Math.ceil(fetchedData.meta.total / pageSize));
     }
   }, [fetchedData, error]);
+
+  useEffect(() => {
+    // Эмулируем задержку для демонстрации скелетона
+    const timer = setTimeout(() => {
+      setIsLoadingUser(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleDelete = async () => {
     setIsDeleteLoading(true);
@@ -75,16 +108,16 @@ export default function MyPostsPage() {
       });
 
       if (!res.ok) {
-        throw new Error('Ошибка при удалении поста');
+        throw new Error(t('failed_delete_post'));
       }
 
-      toast.success('Пост успешно удален!', {
+      toast.success(t('success_delete_post'), {
         position: 'top-right',
       });
 
       blogsMutate();
     } catch (error) {
-      toast.error('Ошибка при удалении поста', {
+      toast.error(t('failed_delete_post'), {
         position: 'top-right',
       });
     }
@@ -106,16 +139,16 @@ export default function MyPostsPage() {
       });
 
       if (!res.ok) {
-        throw new Error('Ошибка при архивации поста');
+        throw new Error(t('failed_archive_post'));
       }
 
-      toast.success('Пост успешно заархивирован!', {
+      toast.success(t('success_archive_post'), {
         position: 'top-right',
       });
 
       blogsMutate();
     } catch (error) {
-      toast.error('Ошибка при архивации поста', {
+      toast.error(t('failed_archive_post'), {
         position: 'top-right',
       });
     }
@@ -124,6 +157,7 @@ export default function MyPostsPage() {
     setShowArchiveModal(false);
   };
 
+  // Открытие модального окна для конвертации аккаунта в продавца
   const openConversionModal = () => {
     setIsConversionModalOpen(true);
   };
@@ -133,6 +167,7 @@ export default function MyPostsPage() {
   };
 
   const handleConvertToSeller = async () => {
+    // Логика отправки запроса на конвертацию аккаунта
     try {
       const response = await fetch('/api/convert-to-seller', {
         method: 'POST',
@@ -149,74 +184,137 @@ export default function MyPostsPage() {
         toast.error('Ошибка при отправке запроса');
       }
     } catch (error) {
+      console.error('Ошибка при отправке запроса', error);
       toast.error('Ошибка при отправке запроса');
     }
   };
 
+  if (isLoadingUser) {
+    // Пока загружается пользовательская информация, показываем скелетон
+    return (
+      <div className='container mx-auto py-6'>
+        <CustomSkeleton />
+        <CustomSkeleton />
+        <CustomSkeleton />
+      </div>
+    );
+  }
+
   return (
-    <div className='mb-[0px] max-w-full px-0 py-6 pb-0 md:mb-[0px]'>
+    <div className='container mx-auto py-6'>
       {user?.seller ? (
-        <>
-          {/* Отображение интерфейса для продавца */}
+        <div className='mb-[0px] w-full px-0 pb-4 md:mb-[0px]'>
           <div className='px-0 py-4'>
             <StreamingCreateModal onCreate={() => {}} isLoading={false}>
               <Button className='btn btn--wide !m-0 flex w-full !rounded-md text-primary text-white'>
                 <CiStreamOn className='mr-2 mt-0' size={20} />
-                Начать стрим
+                {tt('start_stream')}
               </Button>
             </StreamingCreateModal>
           </div>
-
           <div className='mb-4 flex w-full flex-col-reverse items-center justify-between gap-2 sm:flex-row'>
             <div className='relative w-full sm:w-80'>
               <Search className='absolute inset-y-0 left-3 my-auto size-4 text-gray-500' />
-              <Input type='text' placeholder='Поиск' className='pl-12 pr-4' />
+              <Input
+                type='text'
+                placeholder={t('search')}
+                className='pl-12 pr-4'
+              />
             </div>
             <div className='flex w-full items-center justify-between gap-1 sm:w-auto'>
               <div className='grid h-9 grid-cols-2 gap-2 rounded-lg bg-background p-1 px-2'>
-                <Button className='h-7'>Все посты</Button>
-                <Button className='h-7'>Архив</Button>
+                <Button
+                  className='h-7 bg-background text-xs text-inherit shadow-none hover:bg-primary/10 data-[state=active]:bg-primary/10 data-[state=active]:text-primary'
+                  data-state={
+                    searchParams.get('isArchive') === 'true' ? '' : 'active'
+                  }
+                  onClick={() => {
+                    const newSearchParams = new URLSearchParams(searchParams);
+
+                    if (searchParams.get('isArchive') === 'true') {
+                      newSearchParams.delete('isArchive');
+                      newSearchParams.set('page', '1');
+                      router.push(`?${newSearchParams.toString()}`);
+                    }
+                  }}
+                >
+                  {t('all')}
+                </Button>
+                <Button
+                  className='h-7 bg-background text-xs text-inherit shadow-none hover:bg-primary/10 data-[state=active]:bg-primary/10 data-[state=active]:text-primary'
+                  data-state={
+                    searchParams.get('isArchive') === 'true' ? 'active' : ''
+                  }
+                  onClick={() => {
+                    const newSearchParams = new URLSearchParams(searchParams);
+                    if (!searchParams.get('isArchive')) {
+                      newSearchParams.set('isArchive', 'true');
+                      newSearchParams.set('page', '1');
+                      router.push(`?${newSearchParams.toString()}`);
+                    }
+                  }}
+                >
+                  {t('archive')}
+                </Button>
               </div>
               <NewPostModal mutate={blogsMutate}>
                 <Button className='btn btn--wide !m-0 !rounded-md'>
                   <MdOutlinePostAdd className='mr-2 size-5' />
-                  Новый пост
+                  {t('new_post')}
                 </Button>
               </NewPostModal>
             </div>
           </div>
 
-          <div>
-            <ConfirmModal
-              isOpen={showDeleteModal}
-              onClose={() => setShowDeleteModal(false)}
-              title='Вы уверены?'
-              description='Вы уверены, что хотите удалить этот пост?'
-              onConfirm={handleDelete}
-              loading={isDeleteLoading}
-            />
-            <ConfirmModal
-              isOpen={showArchiveModal}
-              onClose={() => setShowArchiveModal(false)}
-              title='Вы уверены?'
-              description='Вы уверены, что хотите архивировать этот пост?'
-              onConfirm={handleArchive}
-              loading={isArchiveLoading}
-            />
-          </div>
-
+          {/* Основной контент */}
           {!error ? (
             fetchedData && blogs ? (
               blogs?.length > 0 ? (
                 <div className='w-full rounded-md p-0'>
                   {blogs.map((blog) => (
-                    <PostCard key={blog.id} {...blog} />
+                    <PostCard
+                      key={blog.id}
+                      id={blog.id}
+                      title={blog.title}
+                      original_title={blog.original_title}
+                      subtitle={blog.subtitle}
+                      original_subtitle={blog.original_subtitle}
+                      content={blog.content}
+                      original_content={blog.original_content}
+                      hashtags={blog.hashtags}
+                      expireDate={blog.expireDate}
+                      cities={blog.cities}
+                      categories={blog.categories}
+                      gallery={blog.gallery}
+                      archived={blog.archived}
+                      price={blog.price}
+                      link={blog.link}
+                      onArchive={() => {
+                        setArchiveID(blog.id);
+                        setShowArchiveModal(true);
+                      }}
+                      onDelete={() => {
+                        setDeleteID(blog.id);
+                        setShowDeleteModal(true);
+                      }}
+                      mutate={blogsMutate}
+                    />
                   ))}
                   {maxPage > 1 && (
                     <PaginationComponent
-                      currentPage={1}
+                      currentPage={
+                        searchParams.get('page')
+                          ? Number(searchParams.get('page'))
+                          : 1
+                      }
                       maxPage={maxPage}
-                      gotoPage={(page) => {}}
+                      gotoPage={(page) => {
+                        const newSearchParams = new URLSearchParams(
+                          searchParams
+                        );
+                        newSearchParams.set('page', page.toString());
+                        router.push(`?${newSearchParams.toString()}`);
+                      }}
                     />
                   )}
                 </div>
@@ -224,7 +322,7 @@ export default function MyPostsPage() {
                 <div className='flex h-60 w-full items-center justify-center rounded-md bg-background/30 p-8'>
                   <div className='flex flex-col items-center text-gray-400'>
                     <MdOutlineSpeakerNotesOff className='size-20' />
-                    Постов пока нет
+                    {t('no_blogs_yet')}
                   </div>
                 </div>
               )
@@ -234,7 +332,29 @@ export default function MyPostsPage() {
           ) : (
             <div></div>
           )}
-        </>
+
+          {/* Модальные окна */}
+          <ConfirmModal
+            isOpen={showDeleteModal}
+            onClose={() => setShowDeleteModal(false)}
+            title={t('are_you_sure')}
+            description={t('are_you_sure_delete_post')}
+            onConfirm={() => {
+              handleDelete();
+            }}
+            loading={isDeleteLoading}
+          />
+          <ConfirmModal
+            isOpen={showArchiveModal}
+            onClose={() => setShowArchiveModal(false)}
+            title={t('are_you_sure')}
+            description={t('are_you_sure_archive_post')}
+            onConfirm={() => {
+              handleArchive();
+            }}
+            loading={isArchiveLoading}
+          />
+        </div>
       ) : (
         <div className='text-center'>
           <h2 className='text-xl font-bold'>
@@ -244,7 +364,7 @@ export default function MyPostsPage() {
             Конвертировать аккаунт в продавца
           </Button>
 
-          {/* Модальное окно для конвертации */}
+          {/* Модальное окно для конвертации аккаунта */}
           <Dialog
             open={isConversionModalOpen}
             onOpenChange={closeConversionModal}
@@ -272,6 +392,17 @@ export default function MyPostsPage() {
           </Dialog>
         </div>
       )}
+    </div>
+  );
+}
+
+export function CustomSkeleton() {
+  return (
+    <div className='space-y-4'>
+      <div className='h-6 w-full animate-pulse rounded-md bg-gray-300'></div>
+      <div className='h-6 w-3/4 animate-pulse rounded-md bg-gray-300'></div>
+      <div className='h-6 w-1/2 animate-pulse rounded-md bg-gray-300'></div>
+      <div className='h-6 w-full animate-pulse rounded-md bg-gray-300'></div>
     </div>
   );
 }
